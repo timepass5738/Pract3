b4b725ff77c3978c09627cc6e4bad131e1f25d96
timepass5738 <timepass5738@gmail.com>
2024-10-24 01:00:15 +0530

    Create clock.py

---
+++ clock.py
@@ -0,0 +1,20 @@
+import sys
+import time
+import datetime
+import RPi.GPIO as GPIO
+import tm1637
+#CLK -> GPIO23 (Pin 16)
+#Di0 -> GPIO24 (Pin 18)
+
+Display = tm1637.TM1637(23,24,tm1637.BRIGHT_TYPICAL)
+Display.Clear()
+Display.SetBrightnes(1)
+while(True):
+   now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    currenttime = [ int(hour / 10), hour % 10, int(minute / 10), minute % 10 ]
+    Display.Show(currenttime)
+    Display.ShowDoublepoint(second % 2)
+    time.sleep(1)
